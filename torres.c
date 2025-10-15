ac0c00cc8ce420823ee3e54aede1b1a76bb6fece
bernttorres-del <bernttorres@gmail.com>
2025-10-15 15:06:59 +0800

    Add files via upload
    uploaded C case problem and output screenshot.

---
+++ torres.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+   printf ("Name: Brent Justin G. Torres\n");
+   printf("ID Number: 2025303884\n");
+   printf("---------------\n");
+
+
+   int brent= 6;
+   int brent2 = 9;
+   int dif;
+   
+   dif = brent - brent2;
+   
+   printf("Result: The difference of %d and %d is %d. \n", brent, brent2, dif);
+   
+   return 0;
+}
